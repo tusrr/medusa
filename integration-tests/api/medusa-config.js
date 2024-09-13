aeeda67752b39463da6f78fbc7cf03eb200fe91a
Tushar Raj <113485857+tusrr@users.noreply.github.com>
2024-09-13 18:07:44 +0530

    updateddd

--- integration-tests/api/medusa-config.js
+++ integration-tests/api/medusa-config.js
@@ -1,26 +1,26 @@
-const { Modules } = require("@medusajs/utils")
+const { Modules } = require("@medusajs/utils");
 
-const DB_HOST = process.env.DB_HOST
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_TEMP_NAME
-const DB_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
-process.env.POSTGRES_URL = DB_URL
-process.env.LOG_LEVEL = "error"
+const DB_HOST = process.env.DB_HOST;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_TEMP_NAME;
+const DB_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+process.env.POSTGRES_URL = DB_URL;
+process.env.LOG_LEVEL = "error";
 
-const enableMedusaV2 = process.env.MEDUSA_FF_MEDUSA_V2 == "true"
+const enableMedusaV2 = process.env.MEDUSA_FF_MEDUSA_V2 === "true";
 
 const customPaymentProvider = {
   resolve: {
     services: [require("@medusajs/payment/dist/providers/system").default],
   },
   id: "default_2",
-}
+};
 
 const customFulfillmentProvider = {
   resolve: "@medusajs/fulfillment-manual",
   id: "test-provider",
-}
+};
 
 module.exports = {
   projectConfig: {
@@ -30,8 +30,6 @@ module.exports = {
     // store_cors: STORE_CORS,
     // admin_cors: ADMIN_CORS,
   },
-  plugins,
-};
   featureFlags: {
     medusa_v2: enableMedusaV2,
   },
@@ -76,7 +74,6 @@ module.exports = {
     [Modules.SALES_CHANNEL]: true,
     [Modules.CART]: true,
     [Modules.WORKFLOW_ENGINE]: true,
-    [Modules.REGION]: true,
     [Modules.API_KEY]: true,
     [Modules.STORE]: true,
     [Modules.TAX]: true,
@@ -84,7 +81,7 @@ module.exports = {
     [Modules.ORDER]: true,
     [Modules.PAYMENT]: {
       resolve: "@medusajs/payment",
-      /** @type {import('@medusajs/payment').PaymentModuleOptions}*/
+      /** @type {import('@medusajs/payment').PaymentModuleOptions} */
       options: {
         providers: [customPaymentProvider],
       },
@@ -111,4 +108,4 @@ module.exports = {
       },
     },
   },
-}
+};
