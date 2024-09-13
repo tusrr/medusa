2cfb1d8785f2ef59443f14d5d80c5c455df1dad2
Tushar Raj <113485857+tusrr@users.noreply.github.com>
2024-09-13 19:13:48 +0530

    modified

--- integration-tests/modules/medusa-config.js
+++ integration-tests/modules/medusa-config.js
@@ -1,55 +1,40 @@
-const { Modules } = require("@medusajs/utils")
-const { FulfillmentModuleOptions } = require("@medusajs/fulfillment")
-const DB_HOST = process.env.DB_HOST
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_NAME = process.env.DB_TEMP_NAME
-const DB_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`
-process.env.POSTGRES_URL = DB_URL
-process.env.LOG_LEVEL = "error"
+const { Modules } = require("@medusajs/utils");
 
-const enableMedusaV2 = process.env.MEDUSA_FF_MEDUSA_V2 == "true"
+const DB_HOST = process.env.DB_HOST;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_TEMP_NAME;
+const DB_URL = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+process.env.POSTGRES_URL = DB_URL;
+process.env.LOG_LEVEL = "error";
+
+const enableMedusaV2 = process.env.MEDUSA_FF_MEDUSA_V2 === "true";
 
 const customPaymentProvider = {
   resolve: {
     services: [require("@medusajs/payment/dist/providers/system").default],
   },
   id: "default_2",
-}
+};
 
 const customFulfillmentProvider = {
   resolve: "@medusajs/fulfillment-manual",
   id: "test-provider",
-}
+};
 
 module.exports = {
-  admin: {
-    disable: true,
-  },
-  plugins: [],
   projectConfig: {
-    databaseUrl: DB_URL,
-    databaseType: "postgres",
-    http: {
-      jwtSecret: "test",
-      cookieSecret: "test",
-    },
+    redis_url: "redis://red-cri0f3tumphs73cav7lg:6379",
+    database_url: "postgresql://medusa_db_9k4q_user:KXb5FEwRmEeB3kxf4iBwtglDyert1mPg@dpg-crhvp9jtq21c7384ar40-a.oregon-postgres.render.com/medusa_db_9k4q",
+    database_type: "postgres",
+    // store_cors: STORE_CORS,
+    // admin_cors: ADMIN_CORS,
   },
   featureFlags: {
     medusa_v2: enableMedusaV2,
   },
   modules: {
-    [Modules.AUTH]: {
-      resolve: "@medusajs/auth",
-      options: {
-        providers: [
-          {
-            id: "emailpass",
-            resolve: "@medusajs/auth-emailpass",
-          },
-        ],
-      },
-    },
+    [Modules.AUTH]: true,
     [Modules.USER]: {
       scope: "internal",
       resources: "shared",
@@ -70,6 +55,17 @@ module.exports = {
       resolve: "@medusajs/inventory-next",
       options: {},
     },
+    [Modules.FILE]: {
+      resolve: "@medusajs/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/file-local-next",
+            id: "local",
+          },
+        ],
+      },
+    },
     [Modules.PRODUCT]: true,
     [Modules.PRICING]: true,
     [Modules.PROMOTION]: true,
@@ -78,7 +74,6 @@ module.exports = {
     [Modules.SALES_CHANNEL]: true,
     [Modules.CART]: true,
     [Modules.WORKFLOW_ENGINE]: true,
-    [Modules.REGION]: true,
     [Modules.API_KEY]: true,
     [Modules.STORE]: true,
     [Modules.TAX]: true,
@@ -86,7 +81,7 @@ module.exports = {
     [Modules.ORDER]: true,
     [Modules.PAYMENT]: {
       resolve: "@medusajs/payment",
-      /** @type {import('@medusajs/payment').PaymentModuleOptions}*/
+      /** @type {import('@medusajs/payment').PaymentModuleOptions} */
       options: {
         providers: [customPaymentProvider],
       },
@@ -113,4 +108,4 @@ module.exports = {
       },
     },
   },
-}
+};
